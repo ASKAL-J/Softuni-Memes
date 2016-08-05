23c45c1771cf9302aaf4583bed567d9777132b3e
redis1966 <programmaker@abv.bg>
2016-08-05 17:18:21 +0300

    StartUp Project

---
+++ Softuni - Memes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Softuni___Memes.Startup))]
+namespace Softuni___Memes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
