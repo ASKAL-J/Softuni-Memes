1b3e3f05e6d7137636c937bc475b7d93a64990ed
Kiril Kirilov <kironakumura@abv.bg>
2016-08-14 13:49:09 +0300

    Revert "Fixed the image model"
    This reverts commit cfbf03697b95700c844456bef59efe6dddbe024a.

--- Softuni - Memes/Models/ImageModel.cs
+++ Softuni - Memes/Models/ImageModel.cs
@@ -4,26 +4,17 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
-using Microsoft.AspNet.Identity;
 
 namespace Softuni___Memes.Models
 {
     [Table("Images")]
     public class ImageModel
     {
-        public ImageModel()
-        {
-            this.AuthorId = HttpContext.Current.User.Identity.GetUserId();
-        }
-
         [Key]
         public int Id { get; set; }
 
         [Column("image", TypeName = "image")]
         public byte[] Image { get; set; }
-
-        [ForeignKey("AuthorId")]
-        public ApplicationUser Author { get; set; }
 
         public string AuthorId { get; set; }
     }
