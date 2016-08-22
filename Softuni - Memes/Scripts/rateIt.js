527eedb75efe407d128b313b383e9716f0619478
John6tain <john6tain.tube@gmail.com>
2016-08-22 15:39:10 +0300

    rating button fixed

--- Softuni - Memes/Scripts/rateIt.js
+++ Softuni - Memes/Scripts/rateIt.js
@@ -1,5 +1,10 @@
-﻿$('.btn').on('click', function () {
+﻿$('.rate').on('click', function () {
     var rankId = $(this).prev().children().last()[0].id;
     var score = $("#" + rankId).val();
     alert(score);
+    /*$.ajax({
+        url: "/",
+        type: "post",
+        data: serializedData
+    })*/
 });
