f7be17c428a2a9ca1762c8053b24d0ed257e2842
redis1966 <programmaker@abv.bg>
2016-08-30 13:51:19 +0300

    Bundle fixed

--- Softuni - Memes/App_Start/BundleConfig.cs
+++ Softuni - Memes/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Softuni___Memes
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css",
+                      "~/Content/Site.css",
                       "~/Content/PagedList.css"));
         }
     }
