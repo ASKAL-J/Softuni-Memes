efeb24a371ea48efb533580df959064760d92962
LusienGG <des7royer.970420@gmail.com>
2016-09-01 00:55:45 +0300

    Surprise for Bat Simo
    Jquery.Mobile.MVC package installed

--- Softuni - Memes/Global.asax.cs
+++ Softuni - Memes/Global.asax.cs
@@ -16,6 +16,7 @@ namespace Softuni___Memes
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            BundleMobileConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
