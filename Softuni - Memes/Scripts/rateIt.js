3b503769f3aa6192bc26f43b43e0176cebf08d32
redis1966 <programmaker@abv.bg>
2016-09-08 03:28:39 +0300

    Some little fixes

--- Softuni - Memes/Scripts/rateIt.js
+++ Softuni - Memes/Scripts/rateIt.js
@@ -2,13 +2,13 @@
     var rankId = $(this).parent().parent().children().first().children().children().last()[0].id;
     var score = $("#" + rankId).val();
     var imageId = rankId.split('-')[1];
-    
+
     $.ajax({
         url: "/Image",
         type: "post",
         data: { rate: score, ImageId: imageId },
-        success: function () {
+        success: function() {
             window.location = "/Image";
         }
-    })
+    });
 });
